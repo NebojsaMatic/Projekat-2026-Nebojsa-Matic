6b8d90233011e3fa5b99b74d4feafc5d4288040d
sanjap14 <123950520+sanjap14@users.noreply.github.com>
2026-03-18 11:07:35 +0100

    V0.3 Baza

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+create database projekat_2026_NebojsaMatic
+use projekat_2026_NebojsaMatic
+create table Korisnik(
+id INT IDENTITY PRIMARY KEY,
+email NVARCHAR(40),
+lozinka NVARCHAR(40),
+uloga INT
+)
