110c6774b42c131ea37ce5a62144539999ac3c2b
sanjap14 <123950520+sanjap14@users.noreply.github.com>
2026-03-18 10:14:26 +0100

    V0.1 Login

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Projekat_2026_Nebojsa_Matic
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
